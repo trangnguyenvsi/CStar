48562fa054919bfc5abe522b5bd20013bd44a19e
thanhvc <thanhvc@thanhvc-w7.vsii.com>
2016-06-13 13:18:23 +0700

    Screening to store verify value

---
+++ src/main/java/com/vsii/cstar/testdata/Screening.java
@@ -0,0 +1,267 @@
+package com.vsii.cstar.testdata;
+
+public class Screening {
+	private String siteName;
+	private String siteType;
+	private String siteAddressType;
+	private String siteAddress1;
+	private String siteAddress2;
+	private String siteAddress3;
+	private String siteZipCode;
+	private String siteCity;
+	private String siteState;
+	private String siteCountry;
+	private String sitePhone;
+	private String siteFax;
+	private String siteEmail;
+	private String siteWebsite;
+	private String contactFirstName;
+	private String contactMIName;
+	private String contactLastName;
+	private String contactType;
+	private String contactPhoneNo;
+	private String contactPhoneExtension;
+	private String contactPhoneType;
+	private String contactEmail;
+	private String contactEmailType;
+	private String representative;
+	private String employeeId;
+	private String marketingPlan;
+	private String ACOType;
+	private String driverType;
+	private String siteOpenTime;
+	private String siteCloseTime;
+	private String productSet;
+	private String priceLevel;
+	private String scheduleCode;
+	private String scheduleTime;
+	private String HIPAAValue;
+	private String screeningId;
+	
+	
+	/**
+	 * Bean Shell
+	 */
+	
+	
+	
+	public String getSiteName() {
+		return siteName;
+	}
+	public String getScreeningId() {
+		return screeningId;
+	}
+	public void setScreeningId(String screeningId) {
+		this.screeningId = screeningId;
+	}
+	public String getContactPhoneExtension() {
+		return contactPhoneExtension;
+	}
+	public void setContactPhoneExtension(String contactPhoneExtension) {
+		this.contactPhoneExtension = contactPhoneExtension;
+	}
+	public String getHIPAAValue() {
+		return HIPAAValue;
+	}
+	public void setHIPAAValue(String hIPAAValue) {
+		HIPAAValue = hIPAAValue;
+	}
+	public String getScheduleTime() {
+		return scheduleTime;
+	}
+	public void setScheduleTime(String scheduleTime) {
+		this.scheduleTime = scheduleTime;
+	}
+	public String getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getSiteType() {
+		return siteType;
+	}
+	public void setSiteType(String siteType) {
+		this.siteType = siteType;
+	}
+	public String getSiteAddressType() {
+		return siteAddressType;
+	}
+	public void setSiteAddressType(String siteAddressType) {
+		this.siteAddressType = siteAddressType;
+	}
+	public String getContactType() {
+		return contactType;
+	}
+	public void setContactType(String contactType) {
+		this.contactType = contactType;
+	}
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
+	public String getSiteAddress1() {
+		return siteAddress1;
+	}
+	public void setSiteAddress1(String siteAddress1) {
+		this.siteAddress1 = siteAddress1;
+	}
+	public String getSiteAddress2() {
+		return siteAddress2;
+	}
+	public void setSiteAddress2(String siteAddress2) {
+		this.siteAddress2 = siteAddress2;
+	}
+	public String getSiteAddress3() {
+		return siteAddress3;
+	}
+	public void setSiteAddress3(String siteAddress3) {
+		this.siteAddress3 = siteAddress3;
+	}
+	public String getSiteZipCode() {
+		return siteZipCode;
+	}
+	public void setSiteZipCode(String siteZipCode) {
+		this.siteZipCode = siteZipCode;
+	}
+	public String getSiteCity() {
+		return siteCity;
+	}
+	public void setSiteCity(String siteCity) {
+		this.siteCity = siteCity;
+	}
+	public String getSiteState() {
+		return siteState;
+	}
+	public void setSiteState(String siteState) {
+		this.siteState = siteState;
+	}
+	public String getSiteCountry() {
+		return siteCountry;
+	}
+	public void setSiteCountry(String siteCountry) {
+		this.siteCountry = siteCountry;
+	}
+	public String getSitePhone() {
+		return sitePhone;
+	}
+	public void setSitePhone(String sitePhone) {
+		this.sitePhone = sitePhone;
+	}
+	public String getSiteFax() {
+		return siteFax;
+	}
+	public void setSiteFax(String siteFax) {
+		this.siteFax = siteFax;
+	}
+	public String getSiteEmail() {
+		return siteEmail;
+	}
+	public void setSiteEmail(String siteEmail) {
+		this.siteEmail = siteEmail;
+	}
+	public String getSiteWebsite() {
+		return siteWebsite;
+	}
+	public void setSiteWebsite(String siteWebsite) {
+		this.siteWebsite = siteWebsite;
+	}
+	public String getContactFirstName() {
+		return contactFirstName;
+	}
+	public void setContactFirstName(String contactFirstName) {
+		this.contactFirstName = contactFirstName;
+	}
+	public String getContactMIName() {
+		return contactMIName;
+	}
+	public void setContactMIName(String contactMIName) {
+		this.contactMIName = contactMIName;
+	}
+	public String getContactLastName() {
+		return contactLastName;
+	}
+	public void setContactLastName(String contactLastName) {
+		this.contactLastName = contactLastName;
+	}
+	public String getContactPhoneNo() {
+		return contactPhoneNo;
+	}
+	public void setContactPhoneNo(String contactPhoneNo) {
+		this.contactPhoneNo = contactPhoneNo;
+	}
+	public String getContactPhoneType() {
+		return contactPhoneType;
+	}
+	public void setContactPhoneType(String contactPhoneType) {
+		this.contactPhoneType = contactPhoneType;
+	}
+	public String getContactEmail() {
+		return contactEmail;
+	}
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+	public String getContactEmailType() {
+		return contactEmailType;
+	}
+	public void setContactEmailType(String contactEmailType) {
+		this.contactEmailType = contactEmailType;
+	}
+	public String getRepresentative() {
+		return representative;
+	}
+	public void setRepresentative(String representative) {
+		this.representative = representative;
+	}
+	public String getMarketingPlan() {
+		return marketingPlan;
+	}
+	public void setMarketingPlan(String marketingPlan) {
+		this.marketingPlan = marketingPlan;
+	}
+	public String getACOType() {
+		return ACOType;
+	}
+	public void setACOType(String aCOType) {
+		ACOType = aCOType;
+	}
+	public String getDriverType() {
+		return driverType;
+	}
+	public void setDriverType(String driverType) {
+		this.driverType = driverType;
+	}
+	public String getSiteOpenTime() {
+		return siteOpenTime;
+	}
+	public void setSiteOpenTime(String siteOpenTime) {
+		this.siteOpenTime = siteOpenTime;
+	}
+	public String getSiteCloseTime() {
+		return siteCloseTime;
+	}
+	public void setSiteCloseTime(String siteCloseTime) {
+		this.siteCloseTime = siteCloseTime;
+	}
+	public String getProductSet() {
+		return productSet;
+	}
+	public void setProductSet(String productSet) {
+		this.productSet = productSet;
+	}
+	public String getPriceLevel() {
+		return priceLevel;
+	}
+	public void setPriceLevel(String priceLevel) {
+		this.priceLevel = priceLevel;
+	}
+	public String getScheduleCode() {
+		return scheduleCode;
+	}
+	public void setScheduleCode(String scheduleCode) {
+		this.scheduleCode = scheduleCode;
+	}
+
+	
+	
+}
